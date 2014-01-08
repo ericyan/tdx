d36017a31052cbb813cfe891ded55719da2d0955
Eric Yan <long@ericyan.me>
2014-01-09 00:26:00 +0800

    Refactor for better encapsulation

--- lib/tdx/parsers/eod.rb
+++ lib/tdx/parsers/eod.rb
@@ -3,30 +3,43 @@ require 'date'
 
 module Tdx
   module Parsers
-    class EoD < BinData::Record
-      endian  :little
+    class EoD
+      def self.parse(file)
+        quotes = []
 
-      uint32  :date       # Date, as yyyymmdd
-      uint32  :open       # Opening price, in cents
-      uint32  :high       # Highiest price, in cents
-      uint32  :low        # Lowest price, in cents
-      uint32  :close      # Closing price, in cents
-      float   :turnover   # Turnover
-      uint32  :volume     # Volume
-      skip    length: 4   # Reserved
+        (file.size / 32).times do |line|
+          file.pos = line * 32
+          quotes << Quote.read(file)
+        end
 
-      def read(data)
-        super(data)
+        return quotes
+      end
 
-        return {
-          date:     Date.parse(date.to_s),
-          open:     open / 100.00,
-          high:     high / 100.00,
-          low:      low / 100.00,
-          close:    close / 100.00,
-          turnover: turnover.to_i,
-          volume:   volume
-        }
+      class Quote < BinData::Record
+        endian  :little
+
+        uint32  :date       # Date, as yyyymmdd
+        uint32  :open       # Opening price, in cents
+        uint32  :high       # Highiest price, in cents
+        uint32  :low        # Lowest price, in cents
+        uint32  :close      # Closing price, in cents
+        float   :turnover   # Turnover
+        uint32  :volume     # Volume
+        skip    length: 4   # Reserved
+
+        def read(data)
+          super(data)
+
+          return {
+            date:     Date.parse(date.to_s),
+            open:     open / 100.00,
+            high:     high / 100.00,
+            low:      low / 100.00,
+            close:    close / 100.00,
+            turnover: turnover.to_i,
+            volume:   volume
+          }
+        end
       end
     end
   end
--- lib/tdx/stock.rb
+++ lib/tdx/stock.rb
@@ -3,13 +3,8 @@ module Tdx
     attr_reader :quotes
 
     def initialize(symbol, range = nil)
-      data = File.open("data/#{symbol}.day", 'rb')
-
-      @quotes = []
-      (data.size / 32).times do |line|
-        data.pos = line * 32
-        @quotes << Parsers::EoD.read(data)
-      end
+      file = File.open("data/#{symbol}.day", 'rb')
+      @quotes = Parsers::EoD.parse(file)
     end
   end
 end
