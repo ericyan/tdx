08d585c22b8e139ef8e0f49aa8846e9c08fd9ef5
Eric Yan <long@ericyan.me>
2014-01-21 00:36:48 +0800

    Return Data::Feed instead of TimeSeries for a sliced feed

--- lib/tdx/data/feed.rb
+++ lib/tdx/data/feed.rb
@@ -3,9 +3,13 @@ require 'time_series'
 module Tdx
   module Data
     class Feed < TimeSeries
-      def initialize(time_step = 240)
+      def initialize(time_step = 240, *args)
         @time_step = time_step
-        super()
+        super(*args)
+      end
+
+      def slice(timeframe)
+        Data::Feed.new(@time_step, super.data_points)
       end
     end
   end
