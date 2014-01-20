8a3098533b825709e7011958a3d684f6ed0abccf
Eric Yan <long@ericyan.me>
2014-01-20 14:27:13 +0800

    Use Yahoo! Finance ticker symbol style

--- lib/tdx/stock.rb
+++ lib/tdx/stock.rb
@@ -1,20 +1,31 @@
 module Tdx
   class Stock
     def initialize(symbol)
-      @symbol = symbol
+      @stock_code, @exchange = parse_symbol(symbol)
     end
 
     def quotes(time_step = 240)
       case time_step
       when 5
-        file = Data::File.open("data/#{@symbol}.5", 'rb')
+        file = Data::File.open("data/#{@exchange.to_s.downcase}#{@stock_code}.5", 'rb')
         @quotes = Parsers::FiveMinutes.parse(file)
       when 240
-        file = Data::File.open("data/#{@symbol}.day", 'rb')
+        file = Data::File.open("data/#{@exchange.to_s.downcase}#{@stock_code}.day", 'rb')
         @quotes = Parsers::EoD.parse(file)
       else
         raise ArgumentError, 'Invalid time step'
       end
     end
+
+    private
+      def parse_symbol(symbol)
+        stock_code, exchange = symbol.split('.')
+        exchange = exchange.to_s.upcase.to_sym
+        unless stock_code =~ /^\d{6}$/ and [:SZ, :SH].include? exchange
+          raise ArgumentError, 'Invalid stock symbol'
+        end
+
+        return [stock_code, exchange]
+      end
   end
 end
