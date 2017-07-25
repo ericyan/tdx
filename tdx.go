4e8756192bd0a471435a81681a86468248ad311d
Eric Yan <github@ericyan.me>
2017-07-25 21:36:18 +0800

    Read five-minute bars

--- tdx.go
+++ tdx.go
@@ -27,3 +27,27 @@ func (day *dayBar) UnmarshalBinary(data []byte) error {
 
 	return nil
 }
+
+// A fiveBar in a single record in five-minute bar (.5) files.
+type fiveBar struct {
+	Date     uint16  // higher 5 bits: years since 2004, higher 11 bits: mmdd
+	Time     uint16  // minutes since 00:00:00 (UTC+8)
+	Open     uint32  // in cents
+	High     uint32  // in cents
+	Low      uint32  // in cents
+	Close    uint32  // in cents
+	Turnover float32 // in yuan
+	Volume   uint32  // in shares
+	Reserved [4]byte // unknown
+}
+
+func (five *fiveBar) UnmarshalBinary(data []byte) error {
+	r := bytes.NewReader(data)
+
+	err := binary.Read(r, binary.LittleEndian, five)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
